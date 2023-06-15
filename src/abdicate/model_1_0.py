24ef147d1022547f5465281d0a92e53a5fdcea91
Tom Verheyen <72074814+VerheyenTom@users.noreply.github.com>
2023-06-15 10:17:59 +0200

    Update model_1_0.py
    Added operational-owners field

--- src/abdicate/model_1_0.py
+++ src/abdicate/model_1_0.py
@@ -48,6 +48,7 @@ class Functional(ExBaseModel):
     domains: Optional[List[str]] = Field(description='List of functional domains this application belongs to.')
     components: Optional[List[str]] = Field(description='List of functional components this application belongs to.')
     cost_centers: Optional[List[str]] = Field(alias='cost-centers', description='List of cost-centers this application belongs to.')
+    operational_owners: Optional[List[str]] = Field(alias='operational-owners', description='List of operational owners this application belongs to.')
 
 
 class Database(Resource):
